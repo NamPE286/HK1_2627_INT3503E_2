3ca0e11f3dfef13606314120abda820550d6e9c5
NamPE286 <nambuihung654@gmail.com>
2026-09-09 15:06:03 +0700

    feat: POST /echo

--- lesson1/app.py
+++ lesson1/app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.post("/echo")
+def echo():
+    data = request.get_json(silent=True) or {}
+    return {"you sent": data}
 
 @app.get("/")
 def index():
